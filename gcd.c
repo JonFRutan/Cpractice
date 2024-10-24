66938e5a26329503929316e6a40eabb4bb275422
JonFRutan <jonfrutan@users.noreply.github.com>
2024-10-24 14:59:37 -0400

    gcd test

---
+++ gcd.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+//Search for two strings GCD.
+//Need, common denominator, size of denomoninator, 
+
+char * gcd_string(char *string1, char *string2) {
+	
+	
+	char *output = "frog";// = (char *)malloc(size * sizeof(char));	
+	return output;
+}
+
+
+int main () {
+	char string1[] = "ABCABCABC";
+	char string2[] = "ABC"; 
+	char *gcd = gcd_string(string1, string2);
+	printf("%s | %s \n", string1, string2);
+	printf("%s\n", gcd);	
+}
+
